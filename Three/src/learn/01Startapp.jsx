6bfc2b8ab81db7065a7cfedc460a58c4cd1478f8
xiqing <qingdiyu1@gmail.com>
2025-02-19 23:47:24 +0800

    更新理解

--- Three/src/learn/01Startapp.jsx
+++ Three/src/learn/01Startapp.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 function Startapp() {
   const containerRef = useRef(null); // 创建容器引用
@@ -9,10 +10,23 @@ function Startapp() {
     const width = containerRef.current.offsetWidth;
     const height = containerRef.current.offsetHeight;
   
-    // 创建场景
-    const scene = new THREE.Scene();
+    //房间 - 3d容器
+    //Scene （场景）是所有3D对象的容器
+    const scene = new THREE.Scene()
+
+    //在房间离放物体
+    //物体：geometry(几何体，就是骨架) + material（材质，就是皮肤）
+    const geometry = new THREE.BoxGeometry()
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    const cube = new THREE.Mesh(geometry, material)
+    //将物体放入房间
+    scene.add(cube)
+
+    //光线 · 照亮物体
+    const light = new THREE.DirectionalLight(0xffffff, 1)
+    scene.add(light)
   
-    // 创建相机
+    // 创建相机, 视角, 宽高比, 近平面, 远平面.相机是我们看到的视角
     const camera = new THREE.PerspectiveCamera(
       75, // 视角
       width / height, // 宽高比
@@ -25,18 +39,13 @@ function Startapp() {
     renderer.setSize(width, height);
     containerRef.current.appendChild(renderer.domElement);
   
-    // 创建几何体
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    // 创建材质
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // 创建网格
-    const cube = new THREE.Mesh(geometry, material);
-  
-    // 将网格添加到场景
-    scene.add(cube);
+
   
     // 设置相机位置
     camera.position.z = 3;
+    //添加世界坐标系辅助
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
   
     // 渲染函数
     const animate = () => {
